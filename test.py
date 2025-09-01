2189eec9fb5d4a698dba6bdc019e5afe5258c002
Piyush12022007 <dhadsepiyush54@gmail.com>
2025-09-01 10:56:01 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-# custom errors in python
-a = int(input("Enter any number btw 5 and 9 : "))
-
-if 5<a<9:
-    print("You have entered a valid number")
-else:
-    raise ValueError("The number is not in the valid range")
